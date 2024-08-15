518f4505fb73f9bc2b5608438241d1d50d5c6b44
sincereman <as@vellit.ru>
2024-08-15 14:52:56 +0300

    add comment direnv

--- 03_1_terraform/start.sh
+++ 03_1_terraform/start.sh
@@ -31,6 +31,12 @@ repo_upgrade: true
 
 EOM
 
+
+# Установлен direnv - перенесено в .envrc
+#export YC_TOKEN=$(yc iam create-token)
+#export YC_CLOUD_ID=$(yc config get cloud-id)
+#export YC_FOLDER_ID=$(yc config get folder-id)
+
 cd tofu
 tofu apply -auto-approve
 
